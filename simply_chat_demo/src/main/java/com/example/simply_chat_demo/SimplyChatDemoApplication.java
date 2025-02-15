e4cbf9179d7a84aad24d7b3ff48cef73db506a6e
Progress Nemaranzhe <140755656+KP-Nemaranzhe@users.noreply.github.com>
2025-02-15 10:05:49 +0200

    added new sring boot files

---
+++ simply_chat_demo/src/main/java/com/example/simply_chat_demo/SimplyChatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.simply_chat_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyChatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyChatDemoApplication.class, args);
+	}
+
+}
