2e54e3d582b06f7dc04587e1f82237233aa8eaff
Progress Nemaranzhe <140755656+KP-Nemaranzhe@users.noreply.github.com>
2025-02-15 11:29:55 +0200

    new xml file

---
+++ simply_chat_demo/src/test/java/com/example/simply_chat_demo/SimplyChatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simply_chat_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyChatDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
