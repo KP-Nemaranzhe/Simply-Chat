98ee587b432cb22749807596d2d60696714067b5
Progress Nemaranzhe <140755656+KP-Nemaranzhe@users.noreply.github.com>
2025-02-18 19:41:54 +0200

    new folder

---
+++ simply_chat_demo/src/main/java/com/example/simply_chat_demo/client/MyStompClient.java
@@ -0,0 +1,33 @@
+package com.example.simply_chat_demo.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+
+public class MyStompClient{
+    //declaration of instant variable
+    private StompSession session;
+    private String username;
+
+    // create the constructor
+    public MyStompClient(String username){
+        this.username = username;
+
+        List<Transport> transports = new ArrayList<>();
+        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+
+        SockJsClient sockJsClient = new SockJsClient(transports);
+        WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+    }    
+
+
+}
